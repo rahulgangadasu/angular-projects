c9251e6345ee4b7781f9232a7c7c04d896233a8d
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-05-11 17:15:57 -0500

    Add Header for the Task Management Angular app

--- first-angular-app/src/app/app.ts
+++ first-angular-app/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('first-angular-app');
