15a8a7ccbdbe61538356a7d7715c5f8dc42bd56d
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-05-15 11:47:35 -0500

    Add signal inputs

--- task-management-app/src/app/user/user.ts
+++ task-management-app/src/app/user/user.ts
@@ -1,6 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
-import { required } from '@angular/forms/signals';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
@@ -10,17 +9,24 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  get imagePath() {
-    return '/assets/users/' + this.avatar;
-  }
+  imagePath = computed(() => '/assets/users/' + this.avatar());
 
   onSelectedUser() {}
 }
 
-/* Using Signals and Computed 
+/* 
+-----> Component Inputs
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+
+
+
+
+
+------> Using Signals and Computed 
 selectedUser = signal(DUMMY_USERS[randomIndex]);
 imagePath = computed(() => '/assets/users/' + this.selectedUser().avatar);
 
