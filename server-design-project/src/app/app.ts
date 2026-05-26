d8f807f09afa6534f3bf5f35bb304281230f5f58
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-05-25 19:52:25 -0500

    Add dashboard item component and new-ticket

--- server-design-project/src/app/app.ts
+++ server-design-project/src/app/app.ts
@@ -3,11 +3,12 @@ import { Header } from './header/header';
 import { ServerStatus } from './dashboard/server-status/server-status';
 import { Traffic } from './dashboard/traffic/traffic';
 import { Tickets } from './dashboard/tickets/tickets';
+import { DashboardItem } from './dashboard/dashboard-item/dashboard-item';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, ServerStatus, Traffic, Tickets],
+  imports: [Header, ServerStatus, Traffic, Tickets, DashboardItem],
   templateUrl: './app.html',
 })
 export class AppComponent {}
