83490f71e7148954d39cd9625dd22f3a3e429bb1
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-05-27 13:04:51 -0500

    Add lifecycle deep dive component with lifecycle hooks and server status updates

--- server-design-project/src/app/dashboard/server-status/server-status.ts
+++ server-design-project/src/app/dashboard/server-status/server-status.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.html',
   styleUrl: './server-status.css',
 })
-export class ServerStatus {
-  currentStatus = 'online';
+export class ServerStatus implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'unknown';
+
+  ngOnInit() {
+    setInterval(() => {
+      const randomValue = Math.random();
+      if (randomValue < 0.3) {
+        this.currentStatus = 'online';
+      } else if (randomValue < 0.6) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
