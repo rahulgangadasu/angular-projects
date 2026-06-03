bc66deb28562bfb00bf8af341b82337e940511ba
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-06-03 18:44:27 -0500

    working with Http get and put requests.

--- tourist-places-project/src/app/places/available-places/available-places.ts
+++ tourist-places-project/src/app/places/available-places/available-places.ts
@@ -1,7 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { Place } from '../place.model';
 import { Places } from '../places';
 import { PlacesContainer } from '../places-container/places-container';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -9,6 +11,63 @@ import { PlacesContainer } from '../places-container/places-container';
   templateUrl: './available-places.html',
   styleUrl: './available-places.css',
 })
-export class AvailablePlaces {
+export class AvailablePlaces implements OnInit {
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+
+  // constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.isFetching.set(true);
+
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places') //,{observe: 'response',//observe: 'events',}
+      .pipe(
+        map((responseData) => responseData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(() => new Error('Something went wrong!'));
+        }),
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        // next: (event) => {
+        //   console.log(event);
+        // },
+        // next: (response) => {
+        //   console.log(response.body?.places);
+        // },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+        error: (err: Error) => {
+          this.error.set(err.message);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+  onSelectPlace(selectedPlace: Place) {
+    this.httpClient
+      .put('http://localhost:3000/user-places', {
+        placeId: selectedPlace.id,
+        title: selectedPlace.title,
+      })
+      .subscribe({
+        next: (responseData) => {
+          console.log(responseData);
+        },
+        complete: () => {
+          console.log('complete');
+        },
+      });
+  }
 }
--- tourist-places-project/src/main.ts
+++ tourist-places-project/src/main.ts
@@ -1,4 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(App).catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [provideHttpClient()],
+}).catch((err) => console.error(err));
