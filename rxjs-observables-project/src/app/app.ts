7b09b27b7ec57702d7753ccee40a81cc48384174
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-06-03 11:47:46 -0500

    working with signals, observables and creating custom observables.

--- rxjs-observables-project/src/app/app.ts
+++ rxjs-observables-project/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
-import { interval, map } from 'rxjs';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,19 +9,62 @@ import { interval, map } from 'rxjs';
   styleUrl: './app.css',
 })
 export class App implements OnInit {
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount); // Converts the signal to an observable
+  // that emits whenever the signal value changes
+  interval$ = interval(3000);
+  intervalSignal = toSignal(this.interval$, { initialValue: 0, manualCleanup: true }); // Converts the observable to a signal
+  // that updates with the latest emitted value
+
+  customInterval$ = new Observable((subscriber) => {
+    let timesEmitted = 0;
+    const interval = setInterval(() => {
+      //subscriber.error(); // Emits an error to demonstrate error handling in the observable
+      if (timesEmitted >= 5) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value from custom interval observable');
+      subscriber.next({
+        message: 'new value emitted',
+      });
+      timesEmitted++;
+    }, 3000);
+  });
+
   private destroyRef = inject(DestroyRef);
 
+  constructor() {
+    // effect(() => {
+    //   console.log('clickCount value changed:', this.clickCount());
+    // });
+  }
+
   ngOnInit() {
-    const subscription = interval(5000)
-      .pipe(map((value) => value * 2)) //modifies the emmited observable value and returns the value
-      .subscribe({
-        next: (value) => console.log(`Emitted value: ${value}`),
-        complete: () => console.log('Observable completed.'),
-        error: (err) => console.error('Observable error:', err),
-      });
+    // const subscription = interval(5000)
+    //   .pipe(map((value) => value * 2)) //modifies the emmited observable value and returns the value
+    //   .subscribe({
+    //     next: (value) => console.log(`Emitted value: ${value}`),
+    //     complete: () => console.log('Observable completed.'),
+    //     error: (err) => console.error('Observable error:', err),
+    //   });
+
+    this.customInterval$.subscribe({
+      next: (value) => console.log(value),
+      complete: () => console.log('Custom interval completed.'),
+    });
+
+    const subscription = this.clickCount$.subscribe({
+      next: (value) => console.log(`clickCount value changed: ${this.clickCount()}`),
+    });
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
+  }
+
+  onClick() {
+    this.clickCount.update((value) => value + 1);
   }
 }
