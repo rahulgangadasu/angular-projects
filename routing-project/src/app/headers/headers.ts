83e5aac831bd03035274581305e73ea8005ceb8a
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-06-29 11:42:23 -0500

    updated version

--- routing-project/src/app/headers/headers.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-header',
-  imports: [],
-  templateUrl: './headers.html',
-  styleUrl: './headers.css',
-})
-export class Header {}
--- routing-project/src/app/tasks/no-task/no-task.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-no-task',
-  imports: [],
-  templateUrl: './no-task.html',
-  styleUrl: './no-task.css',
-})
-export class NoTask {}
